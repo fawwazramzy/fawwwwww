cda89054061db0d347b72a421e6b32979e3f213b
iloops <iloops@172.16.183.131>
2018-10-16 13:24:27 +0700

    new main

---
+++ src/newproject/Newproject.java
@@ -0,0 +1,17 @@
+
+package newproject;
+
+/**
+ *
+ * @author iloops
+ */
+public class Newproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
